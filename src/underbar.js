583e96974d9fc69f1fc05c2ffa08b88538073c3e
Kyle Morehead <krmorehead@gmail.com>
2015-10-14 15:11:14 -0700

    Completed Defaults

--- src/underbar.js
+++ src/underbar.js
@@ -229,11 +229,29 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.reduce(arguments,function(accumulator, properties){
+
+      _.each(properties,function(el, key){
+        obj[key] = el
+      })
+      return obj
+    })
+    return obj
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.reduce(arguments,function(accumulator, properties){
+
+      _.each(properties,function(el, key){
+        if(obj[key] === undefined){
+          obj[key] = el
+        }
+      })
+      return obj
+    })
+    return obj
   };
 
 
